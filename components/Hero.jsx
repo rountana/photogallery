32a019c4be2ed7544fbeb30d3be9fdb4985efa03
rountana <shaam.tx@gmail.com>
2023-05-15 17:27:56 -0500

    mobile menu & navbar scroll changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Hero = ({ heading, message }) => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
+    <div className="flex flex-col items-center justify-center h-screen mb-[10rem] bg-fixed bg-center bg-cover custom-img">
       {/* Overlay & make it transparent to give it a yellow retro tinge*/}
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-yellow-500/20 z-[2]" />
-      <div className="p-5 text-white z-[2] ml-[-40rem] mt-[-30rem]">
+      <div className="p-5 text-white z-[2] ml-0 mt-[-30rem] sm:ml-[-40rem] ">
         <h2 className="text-5xl font-bold"> {heading} </h2>
         <p className="py-5 text-xl">{message}</p>
         <button className="px-8 py-2 border">Book</button>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,25 +1,46 @@
 import React from "react";
 import Link from "next/link";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/Ai";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
   const [menuSelected, setMenuSelected] = useState(false);
+  const [color, setColor] = useState("transparent");
+  const [textColor, setTextColor] = useState("white");
 
   const handleMenu = () => {
     setMenuSelected(!menuSelected);
   };
+
+  useEffect(() => {
+    const changeColor = () => {
+      if (window.scrollY >= 90) {
+        setColor("#ffffff");
+        setTextColor("#000000");
+      } else {
+        setColor("transparent");
+        setTextColor("white");
+      }
+    };
+    window.addEventListener("scroll", changeColor);
+  }, []);
+
   return (
     // tailwind notes: fix the navbar in place.
     // Ease-in duration when we change color of background whenever we scroll
-    <div className="fixed left-0 top-0 w-full z-10 ease-in duration-300">
+    <div
+      style={{ backgroundColor: `${color}` }}
+      className="fixed left-0 top-0 w-full z-10 ease-in duration-300"
+    >
       <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
         {/* for the logo */}
         <Link href="/">
-          <h1 className="font-bold text-4xl"> Header </h1>
+          <h1 style={{ color: `${textColor}` }} className="font-bold text-4xl">
+            Cosmo
+          </h1>
         </Link>
         {/* anything above small size its going to be flex, otherwise will be hidden (for mobile form factor) */}
-        <ul className="hidden sm:flex">
+        <ul style={{ color: `${textColor}` }} className="hidden sm:flex">
           <li className="p-4">
             <Link href="/">Home </Link>
           </li>
@@ -33,33 +54,48 @@ const Navbar = () => {
             <Link href="/contact">Contact </Link>
           </li>
         </ul>
-      </div>
-      {/* Mobile Button */}
 
-      <div onClick={handleMenu} className="justify-end">
-        {!menuSelected ? (
-          <AiOutlineMenu color="white" size={40} />
-        ) : (
-          <AiOutlineClose color="white" size={40} />
-        )}
-      </div>
+        <div onClick={handleMenu} className="z-20 sm:hidden">
+          {!menuSelected ? (
+            <AiOutlineMenu
+              style={{ color: `${textColor}` }}
+              color="white"
+              size={40}
+            />
+          ) : (
+            <AiOutlineClose
+              style={{ color: `${textColor}` }}
+              color="white"
+              size={40}
+            />
+          )}
+        </div>
 
-      {/* Make the app mobile friendly, imported react-icons */}
-      <div className="sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-white text-center ease-in duration-300">
-        <ul>
-          <li className="p-4 text-4xl hover: text-gray-500">
-            <Link href="/">Home </Link>
-          </li>
-          <li className="p-4 text-4xl hover: text-gray-500">
-            <Link href="/#gallery">Gallery </Link>
-          </li>
-          <li className="p-4 text-4xl hover: text-gray-500">
-            <Link href="/portfolio">Work </Link>
-          </li>
-          <li className="p-4 text-4xl hover: text-gray-500">
-            <Link href="/contact">Contact </Link>
-          </li>
-        </ul>
+        {/* Mobile Button */}
+
+        {/* Make the app mobile friendly, imported react-icons */}
+        <div
+          className={
+            menuSelected
+              ? "sm:hidden z-0 absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-white text-center ease-in duration-300"
+              : "sm:hidden z-0 absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-white text-center ease-in duration-300"
+          }
+        >
+          <ul>
+            <li className="p-4 text-4xl hover: text-gray-500">
+              <Link href="/">Home </Link>
+            </li>
+            <li className="p-4 text-4xl hover: text-gray-500">
+              <Link href="/#gallery">Gallery </Link>
+            </li>
+            <li className="p-4 text-4xl hover: text-gray-500">
+              <Link href="/portfolio">Work </Link>
+            </li>
+            <li className="p-4 text-4xl hover: text-gray-500">
+              <Link href="/contact">Contact </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
